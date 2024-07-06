e162046d0ac6e5bc5e8b2e97eef09dda57f1093f
Amir <amirkabdou@gmail.com>
2024-07-06 15:58:56 -0700

    Added Different bet depths for each street for GameTree Config

--- PokerSolver/GameTree.cpp
+++ PokerSolver/GameTree.cpp
@@ -16,29 +16,33 @@ Constructors / Deconstructor definitions
 
 GameTreeConfig::GameTreeConfig() {
 
-	this->OOP_flop_bets = new std::vector<float>;
-	this->OOP_turn_bets = new std::vector<float>;
-	this->OOP_river_bets = new std::vector<float>;
+	this->OOP_flop_bets = new std::vector<std::vector<float>*>;
+	this->OOP_turn_bets = new std::vector<std::vector<float>*>;
+	this->OOP_river_bets = new std::vector<std::vector<float>*>;
 
-	this->IP_flop_bets = new std::vector<float>;
-	this->IP_turn_bets = new std::vector<float>;
-	this->IP_river_bets = new std::vector<float>;
+	this->IP_flop_bets = new std::vector<std::vector<float>*>;
+	this->IP_turn_bets = new std::vector<std::vector<float>*>;
+	this->IP_river_bets = new std::vector<std::vector<float>*>;
+
 
 }
 
 GameTreeConfig::GameTreeConfig(
-	std::vector<float> oop_flop_bets, std::vector<float> oop_turn_bets,
-	std::vector<float> oop_river_bets, std::vector<float> ip_flop_bets,
-	std::vector<float> ip_turn_bets, std::vector<float> ip_river_bets) {
+	std::vector< std::vector<float>* > oop_flop_bets, std::vector< std::vector<float>* > oop_turn_bets,
+	std::vector< std::vector<float>* > oop_river_bets, std::vector< std::vector<float>* > ip_flop_bets,
+	std::vector< std::vector<float>* > ip_turn_bets, std::vector< std::vector<float>* > ip_river_bets,
+	float effective_stack) {
 
 	GameTreeConfig();
-	SetOopFlopBets(OOP_flop_bets);
-	SetOopTurnBets(OOP_turn_bets);
-	SetOopRiverBets(OOP_river_bets);
+	SetOopFlopBets(&oop_flop_bets);
+	SetOopTurnBets(&oop_turn_bets);
+	SetOopRiverBets(&oop_river_bets);
 
-	SetIpFlopBets(IP_flop_bets);
-	SetIpTurnBets(IP_turn_bets);
-	SetIpRiverBets(IP_turn_bets);
+	SetIpFlopBets(&ip_flop_bets);
+	SetIpTurnBets(&ip_turn_bets);
+	SetIpRiverBets(&ip_river_bets);
+
+	this->effective_stack_size = effective_stack;
 
 }
 
@@ -58,66 +62,94 @@ GameTreeConfig::~GameTreeConfig() {
 Settor API definitions
 */
 
-void GameTreeConfig::SetOopFlopBets(std::vector<float> oop_flop_bets) {
+void GameTreeConfig::SetOopFlopBets(std::vector< std::vector<float>* >* oop_flop_bets) {
 
-	std::move(oop_flop_bets.begin(), oop_flop_bets.end(), this->OOP_flop_bets->begin());
+
+	for (size_t i = 0; i < oop_flop_bets->size(); i++) {
+
+		oop_flop_bets->at(i) = new std::vector<float>;
+		std::move(oop_flop_bets->at(i)->begin(), oop_flop_bets->at(i)->end(), this->OOP_flop_bets->at(i)->begin());
+
+	}
 }
 
-void GameTreeConfig::SetOopTurnBets(std::vector<float> oop_turn_bets) {
+void GameTreeConfig::SetOopTurnBets(std::vector< std::vector<float>* >* oop_turn_bets) {
 
-	std::move(oop_turn_bets.begin(), oop_turn_bets.end(), this->OOP_turn_bets->begin());
+	for (size_t i = 0; i < oop_turn_bets->size(); i++) {
+
+		oop_turn_bets->at(i) = new std::vector<float>;
+		std::move(oop_turn_bets->at(i)->begin(), oop_turn_bets->at(i)->end(), this->OOP_turn_bets->at(i)->begin());
+	}
+	
 }
 
-void GameTreeConfig::SetOopRiverBets(std::vector<float> oop_river_bets) {
+void GameTreeConfig::SetOopRiverBets(std::vector< std::vector<float>* >* oop_river_bets) {
 
-	std::move(oop_river_bets.begin(), oop_river_bets.end(), this->OOP_river_bets->begin());
+	for (size_t i = 0; i < oop_river_bets->size(); i++) {
+
+		oop_river_bets->at(i) = new std::vector<float>;
+		std::move(oop_river_bets->at(i)->begin(), oop_river_bets->at(i)->end(), this->OOP_river_bets->at(i)->begin());
+	}
 }
 
-void GameTreeConfig::SetIpFlopBets(std::vector<float> ip_flop_bets) {
+void GameTreeConfig::SetIpFlopBets(std::vector< std::vector<float>* >* ip_flop_bets) {
 
-	std::move(ip_flop_bets.begin(), ip_flop_bets.end(), this->IP_flop_bets->begin());
+	for (size_t i = 0; i < ip_flop_bets->size()); i++) {
+
+		ip_flop_bets->at(i) = new std::vector<float>;
+		std::move(ip_flop_bets->at(i)->begin(), ip_flop_bets->at(i)->end(), this->IP_flop_bets->at(i)->begin());
+	}
+	
 }
 
-void GameTreeConfig::SetIpTurnBets(std::vector<float> ip_turn_bets) {
+void GameTreeConfig::SetIpTurnBets(std::vector< std::vector<float>* >* ip_turn_bets) {
 
-	std::move(ip_turn_bets.begin(), ip_turn_bets.end(), this->IP_turn_bets->begin());
+	for (size_t i = 0; i < ip_turn_bets->size(); i++) {
+
+		ip_turn_bets->at(i) = new std::vector<float>;
+		std::move(ip_turn_bets->at(i)->begin(), ip_turn_bets->at(i)->end(), this->IP_turn_bets->at(i)->begin());
+	}
 }
 
-void GameTreeConfig::SetIpRiverBets(std::vector<float> ip_river_bets) {
+void GameTreeConfig::SetIpRiverBets(std::vector< std::vector<float>* >* ip_river_bets) {
 
-	std::move(ip_river_bets.begin(), ip_river_bets.end(), this->IP_river_bets->begin());
+	for (size_t i = 0; i < ip_river_bets->size(); i++) {
+
+		ip_river_bets->at(i) = new std::vector<float>;
+		std::move(ip_river_bets->at(i)->begin(), ip_river_bets->at(i)->end(), this->IP_river_bets->at(i)->begin());
+	}
 }
 
 /*
 Retrieval API definitions
 */
 
-std::vector<float>* GameTreeConfig::GetOopFlopBets() {
+std::vector< std::vector<float>* >* GameTreeConfig::GetOopFlopBets() {
 
 	return this->OOP_flop_bets;
 }
 
-std::vector<float>* GameTreeConfig::GetOopTurnBets() {
+std::vector< std::vector<float>* >* GameTreeConfig::GetOopTurnBets() {
 
 	return this->OOP_turn_bets;
 }
 
-std::vector<float>* GameTreeConfig::GetOopRiverBets() {
+std::vector< std::vector<float>* >* GameTreeConfig::GetOopRiverBets() {
 
 	return this->OOP_river_bets;
 }
 
-std::vector<float>* GameTreeConfig::GetIpFlopBets() {
+std::vector< std::vector<float>* >* GameTreeConfig::GetIpFlopBets() {
 
 	return this->IP_flop_bets;
 }
 
-std::vector<float>* GameTreeConfig::GetIpTurnBets() {
+std::vector< std::vector<float>* >* GameTreeConfig::GetIpTurnBets() {
 
 	return this->IP_turn_bets;
 }
 
-std::vector<float>* GameTreeConfig::GetIpRiverBets() {
+std::vector< std::vector<float>* >* GameTreeConfig::GetIpRiverBets() {
 
 	return this->IP_river_bets;
 }
--- PokerSolver/GameTree.h
+++ PokerSolver/GameTree.h
@@ -8,13 +8,26 @@ class GameTreeConfig {
 
 private:
 
-	std::vector<float> *OOP_flop_bets;
-	std::vector<float> *OOP_turn_bets;
-	std::vector<float> *OOP_river_bets;
+	/*
+	Nested vector stores bet sizes used for each street by each player.
+	Nested vector is indexed by depth of tree for current street.
+		For example: 
+			0th index is reserved for being first player to have aggressive action (bet)
+			2nd index is used for 4-bet at current street with following action:
+				4-bet for IP player on flop:
+					1st Action: IP player bets						(IP_flop_bets[0])
+					2nd Action: OOP player raises (3-bets)			(OOP_flop_bets[1])
+					3rd Action: IP player raises again (4-bets)		(IP_flop_bets[2])
+	*/
+	std::vector< std::vector<float>* > *OOP_flop_bets;
+	std::vector< std::vector<float>* > *OOP_turn_bets;
+	std::vector< std::vector<float>* > *OOP_river_bets;
 
-	std::vector<float> *IP_flop_bets;
-	std::vector<float> *IP_turn_bets;
-	std::vector<float> *IP_river_bets;
+	std::vector< std::vector<float>* > *IP_flop_bets;
+	std::vector< std::vector<float>* > *IP_turn_bets;
+	std::vector< std::vector<float> *> *IP_river_bets;
+
+	float effective_stack_size;
 	
 
 public:
@@ -27,12 +40,13 @@ public:
 
 	//Creates GameTreeConfig and sets bet sizes for each player for each street.
 	GameTreeConfig(
-		std::vector<float> OOP_flop_bets,
-		std::vector<float> OOP_turn_bets,
-		std::vector<float> OOP_river_bets,
-		std::vector<float> IP_flop_bets,
-		std::vector<float> IP_turn_bets,
-		std::vector<float> IP_river_bets
+		std::vector< std::vector<float>* > OOP_flop_bets,
+		std::vector< std::vector<float>* > OOP_turn_bets,
+		std::vector< std::vector<float>* > OOP_river_bets,
+		std::vector< std::vector<float>* > IP_flop_bets,
+		std::vector< std::vector<float>* > IP_turn_bets,
+		std::vector< std::vector<float>* > IP_river_bets,
+		float effective_stack
 	);
 
 	~GameTreeConfig();
@@ -45,34 +59,34 @@ public:
 	/*
 	Game Tree Settor API
 	*/
-	void SetOopFlopBets(std::vector<float> OOP_flop_bets);
+	void SetOopFlopBets(std::vector< std::vector<float>* >* OOP_flop_bets);
 
-	void SetOopTurnBets(std::vector<float> OOP_turn_bets);
+	void SetOopTurnBets(std::vector< std::vector<float>* >* OOP_turn_bets);
 
-	void SetOopRiverBets(std::vector<float> OOP_river_bets);
+	void SetOopRiverBets(std::vector< std::vector<float>* >* OOP_river_bets);
 
-	void SetIpFlopBets(std::vector<float> IP_flop_bets);
+	void SetIpFlopBets(std::vector< std::vector<float>* >* IP_flop_bets);
 
-	void SetIpTurnBets(std::vector<float> IP_turn_bets);
+	void SetIpTurnBets(std::vector< std::vector<float>* >* IP_turn_bets);
 
-	void SetIpRiverBets(std::vector<float> IP_river_bets);
+	void SetIpRiverBets(std::vector< std::vector<float>* >* IP_river_bets);
 
 	
 	/*
 	Game Tree Retrieval API
 	*/
 
-	std::vector<float>* GetOopFlopBets();
+	std::vector< std::vector<float>* >* GetOopFlopBets();
 
-	std::vector<float>* GetOopTurnBets();
+	std::vector< std::vector<float>* >* GetOopTurnBets();
 
-	std::vector<float>* GetOopRiverBets();
+	std::vector< std::vector<float>* >* GetOopRiverBets();
 
-	std::vector<float>* GetIpFlopBets();
+	std::vector< std::vector<float>* >* GetIpFlopBets();
 
-	std::vector<float>* GetIpTurnBets();
+	std::vector< std::vector<float>* >* GetIpTurnBets();
 
-	std::vector<float>* GetIpRiverBets();
+	std::vector< std::vector<float>* >* GetIpRiverBets();
 
 };
 
@@ -82,7 +96,8 @@ public:
 
 	GameStateNode* root;
 
-	GameTree();
+	//Constructs static game tree with GameTreeConfiguration
+	GameTree(GameTreeConfig*);
 
 	GameStateNode* GetRootGameState;
 	
